4a024cc307a6e163d7d633828d665e37c20c67d4
Alex Abrego <alexabrego347@gmail.com>
2022-06-04 20:55:29 -0500

    Mi primer commit:

---
+++ IdentityManager/Archivo.cs
@@ -0,0 +1,11 @@
+usig.System;
+namespace Listas
+{
+    public class Materias{
+        public static void Main(){
+        
+        Console.WriteLine("hola papu");
+        console.Readline();
+        }
+    }
+}
